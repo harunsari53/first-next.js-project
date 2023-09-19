b4a109063bdbd4769d3c35fff7200a4a09b6259f
harunsari53 <harunsari2303@gmail.com>
2023-09-19 10:12:18 +0300

    first commit

---
+++ app/users/page.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+interface IUser {
+  id: number;
+  name: string;
+  email: string;
+}
+
+const UsersPage = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users", {
+    cache: "no-store",
+    //next: { revalidate: 10 },
+    //next js run at background & get data from backend every 10 sec. ('cache'ing in next.js)}
+  });
+  const users: IUser[] = await res.json();
+
+  return (
+    <>
+      <h1>Users</h1>
+      <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
+};
+
+export default UsersPage;
